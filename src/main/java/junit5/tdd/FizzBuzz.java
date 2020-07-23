25c2f5883ff1e3eb318c0396d70ff504d1118fcf
jenkincstang <jenkincs@163.com>
2020-07-23 11:52:58 +0800

    [FizzBuzz] [Jnekin] refactor: refactor play method in Class FizzBuzz

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         if (inputNumber % 3 == 0){
             return "Fizz";
         }
-        if(inputNumber == 5){
+        if(inputNumber % 5 == 0){
             return "Buzz";
         }
         return ""+inputNumber;
